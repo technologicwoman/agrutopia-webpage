71b3c55c074cd0eb916890014d9dc85ec477d417
Laura Angelica <lacardenas.code@gmail.com>
2025-04-06 23:19:55 -0300

    🎨⬆️ feat: upgrade tailwindcss and astro, mobile menu with animations and color and fonts theme configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  experimental: {
+    svg: true
+  }
 });
